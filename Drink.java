d8702d4642d6a32f1a1b6c968494eb364e6f91e6
codyhurley <codyrhurley@gmail.com>
2017-01-30 10:11:07 -0600

    Drink assignment

---
+++ Drink.java
@@ -0,0 +1,18 @@
+
+public class Drink {
+
+	public static void main(String[] args) {
+		
+		int survey = 12467;
+		int percentBuy = 14;
+		int percentCitrus = 64;
+		
+		System.out.print("Out of a survey of " + survey);
+		System.out.print(" people, " + percentBuy);
+		System.out.println("% buy energy drinks.");
+		System.out.print("Of that number, " + percentCitrus);
+		System.out.println("% prefer the citrus-flavored drinks.");
+		
+	}
+
+}
