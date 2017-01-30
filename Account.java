455c4918344cdaa1483cb506bbbd34c5500b49c3
codyhurley <codyrhurley@gmail.com>
2017-01-30 09:57:30 -0600

    Account assignment

---
+++ Account.java
@@ -0,0 +1,13 @@
+
+public class Account {
+
+	public static void main(String[] args) {
+		
+		String name = "John Black";
+		double balance = 1000.0;
+		System.out.println ("Account holder is " + name);
+		System.out.println("Account balance is " + balance);
+
+	}
+
+}
