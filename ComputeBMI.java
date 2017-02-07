485b1d7a508ea6923e65c2d59aac9e6c7825c170
aktunc-STMU <aktunc@gmail.com>
2017-02-06 20:46:03 -0600

    Check comments, missing code

--- ComputeBMI.java
+++ ComputeBMI.java
@@ -17,6 +17,10 @@ public class ComputeBMI {
 		inputHeight =
 		        JOptionPane.showInputDialog("Please enter your height " + "in inches. ");
 		
+		//You have not parsed the information you received from the user to height and weight.
+		//At this point weight and height are still 0.
+		//Please check the example in your slides how to parse from String to int.
+		
 		// Calculate BMI
 		
 		BMI = (double) (weight * 0.45359237) / ((height * 0.0254) * (height * 0.0254));
