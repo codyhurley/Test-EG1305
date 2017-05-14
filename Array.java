5595e5cd210d369578b72b28a2595525f92ec87a
aktunc-STMU <aktunc@gmail.com>
2017-05-14 17:15:11 -0500

    OK. Graded this.

--- Array.java
+++ Array.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class Array {
-
 	public static void main(String[] args) {
 		
 		System.out.println("This program will find the highest, lowest, average, and sum of the numbers you will enter.");
